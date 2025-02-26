5ac2e9103018215751f7abf3fa4f7537f3b33eb6
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-02-26 22:23:51 +0530

    feat :event

--- backend/db.js
+++ backend/db.js
@@ -204,3 +204,51 @@ app.post("/login", (req, res) => {
         }
     });
 });
+
+
+app.post("/signup", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10); 
+        const sql = "INSERT INTO login (name, email, password, role) VALUES (?, ?, ?, 'user')";
+
+        db.query(sql, [name, email, hashedPassword], (err, data) => {
+            if (err) {
+                console.error("Database query error:", err);
+                return res.status(500).json({ message: "Database error", error: err.message });
+            }
+            return res.status(200).json({ message: "User registered successfully" });
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Error hashing password", error: error.message });
+    }
+});
+
+
+app.post("/events",async (req,res)=>{
+    const {title,date,description,venue,total_tickets,price} = req.body;
+
+    if(!title || !date || !description || !venue || !total_tickets || !price){
+        return res.status(400).json({message:"All fields are Required"})
+    }
+
+    try{
+        const sql = "INSERT INTO events (title,date,description,venue,total_tickets,price) VALUES (?,?,?,?,?,?)"
+
+        db.query(sql,[title,date,description,venue,total_tickets,price],(err,result)=>{
+
+            if(err){
+                console.error("Database Query Error",err)
+                return res.status(500).json({message:"Database Error",error:err.message})
+            }
+            return res.status(200).json({message:"Event Created Successfully"});
+        });
+    } catch(error){
+        return res.status(500).json({message:"error"})
+    }
+})
